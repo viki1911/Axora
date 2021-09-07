ab4733e54fefac3c3aff9b54671d184a7cc1870b
viki1911 <viki1911@users.noreply.github.com>
2021-09-07 17:03:15 +0100

    Updaitng login file

--- build/login_to_org.sh
+++ build/login_to_org.sh
@@ -3,6 +3,6 @@ mkdir keys
 echo $CERT_KEY | base64 -di > keys/server.key
 
 echo "Authenticating org"
-sfdx force:auth:jwt:grant --clientid $APP_KEY --jwtkeyfile keys/server.key --username $SF_USERNAME --setdefaultdevhubusername -a DevHub
+sfdx force:auth:jwt:grant --clientid $APP_KEY --jwtkeyfile keys/server.key --instanceurl "https://login.salesforce.com" --username $SF_USERNAME --setdefaultdevhubusername -a DevHub 
 
-echo "Authenticated..."
+echo "Authenticated..." 
